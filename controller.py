c92c838dc7c9d5062af5902068221f4883bcd7a8
Iflier <alexalf@163.com>
2018-09-14 22:56:40 +0800

    Add re lib to filter command.

--- controller.py
+++ controller.py
@@ -2,10 +2,13 @@
 """
 Dec: 连接本地串口的蓝牙主机，控制从机#1和从机#2
 Created on: 2018.09.12
+Modified on: 2018.09.14
+添加正则表达式，过滤来自console的输入命令
 Author: Iflier
 """
 print(__doc__)
 
+import re
 import os
 import sys
 import time
@@ -20,10 +23,11 @@ import serial
 ap = argparse.ArgumentParser(description="开两个线程，一个线程接受console输入，直接向各个设备发送命令；另一个是受到另一个线程控制的线程")
 ap.add_argument("-p", "--port", type=str, default="COM6", help="Specify a serial port to connect, such as COM6, ...")
 ap.add_argument("-b", "--baudrate", type=int, default=9600, help="Specify a baud rate, such as 9600, 115200, ...")
+# ap.add_argument("-d", "--debug", type=bool, default=False, help="Weather enter into debug mode.")
 args = vars(ap.parse_args())
 
 eventObj = Event()
-CPUCORES = psutil.cpu_count(logical=True)
+CPUCORES = psutil.cpu_count(logical=True)  # 包含逻辑CPU个数
 com = serial.Serial(port=args["port"], baudrate=args["baudrate"], timeout=7)
 
 
@@ -38,20 +42,20 @@ def autoRunMode(serialObj, eventObj):
             # 处理器核心的平均使用率
             avergeSystemUtilization = math.ceil(systemUtilization / CPUCORES)
             if avergeSystemUtilization <= 10:
-                fanSpeed = 20
-            elif 10 < avergeSystemUtilization and avergeSystemUtilization <= 20:
                 fanSpeed = 25
-            elif 20 < avergeSystemUtilization and avergeSystemUtilization <= 30:
+            elif 10 < avergeSystemUtilization and avergeSystemUtilization <= 20:
                 fanSpeed = 35
-            elif  30 < avergeSystemUtilization and avergeSystemUtilization <= 40:
+            elif 20 < avergeSystemUtilization and avergeSystemUtilization <= 30:
                 fanSpeed = 50
-            elif 40 < avergeSystemUtilization and avergeSystemUtilization <= 50:
+            elif  30 < avergeSystemUtilization and avergeSystemUtilization <= 40:
                 fanSpeed = 60
-            elif avergeSystemUtilization > 50:
+            elif 40 < avergeSystemUtilization and avergeSystemUtilization <= 50:
                 fanSpeed = 75
+            elif avergeSystemUtilization > 50:
+                fanSpeed = 90
             else:
                 fanSpeed = 0
-            _ = serialObj.write(('2#' + str(fanSpeed) + ';').encode())  # 分号作为串口结束符
+            _ = serialObj.write(('N,2#' + str(fanSpeed) + ';').encode())  # 不要求应答，分号作为串口结束符
             time.sleep(6.0)
         else:
             eventObj.wait()  # 阻塞
@@ -59,7 +63,7 @@ def autoRunMode(serialObj, eventObj):
     sys.exit(0)
 
 def acceptCommandMode(serialObj, eventObj):
-    # 包装后的命令以S开头表示发送的是字符串，要求字符串命令串口最好是有返回
+    # 包装后的命令以 'R' 开头表示要求有应答，以 'N' 开头的命令不需要应答
     if serialObj.is_open:
         while True:
             command = input("Command -->:").lower()  # 阻塞
@@ -70,22 +74,23 @@ def acceptCommandMode(serialObj, eventObj):
                 eventObj.set()
             elif command == "cancel":
                 eventObj.clear()
-            else:
-                if command in ['a', 'p', 't']:
-                    _ = serialObj.write(('1#' + command + ';').encode())
+            elif command in ['a', 'p', 't']:
+                _ = serialObj.write(('R,1#' + command + ';').encode())  # 要求有应答
+                result = serialObj.readline().decode()
+                print("Response -->: {0}".format(result))
+            elif bool(re.match(r"[1-9]?\d?$|100$", command)):
+                if not eventObj.is_set():
+                    # 未进入auto模式下，允许从console控制fan
+                    _ = serialObj.write(('R,2#' + command + ';').encode())  # 要求有应答
                     result = serialObj.readline().decode()
                     print("Response -->: {0}".format(result))
-                elif 0 <= int(float(command)) <= 100:
-                    if not eventObj.is_set():
-                        # 未进入auto模式下，允许从console控制fan
-                        _ = serialObj.write(('2#' + command + ';').encode())
-                        result = serialObj.readline().decode()
-                        print("Response -->: {0}".format(result))
-                    else:
-                        print("[WARN] Fan in auto mode, exit from it please.")
                 else:
-                    print("[ERROR] Unknown command.")
+                    # 如果已经进入到Auto Run模式
+                    print("[WARNNING] Fan in auto mode, exit from it please.")
+            else:
+                print("[ERROR] Received an unknown command: {0}".format(command))
         serialObj.close()
+        sys.exit(0)
     else:
         print("[ERROR] Failed to open serial port.")
         sys.exit(0)
